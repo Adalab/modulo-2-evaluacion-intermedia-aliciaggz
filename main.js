5340d765c7ddad22dc03d7f99c7e0f36ab1c2248
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2021-12-14 09:57:31 +0100

    add game

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 'use strict'
 
 const select = document.querySelector('.js-select');
-const button = document.querySelector('js-btn');
+const button = document.querySelector('.js-btn');
 const player = document.querySelector('.js-player');
 const pc = document.querySelector('.js-pc');
+const response = document.querySelector('.js-response');
+
+
+
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 };
 
-
+const randomNumber = getRandomNumber(10);
 
 function pcPlay(){
-    const randomNumber = getRandomNumber(10);
+
     console.log(randomNumber);
 
-    if(randomNumber < 3){
+    if(randomNumber <= 3){
         console.log('Ordenador: Piedra')
-    }else if (randomNumber >= 6){
+    }else if (randomNumber <= 6){
         console.log('Ordenador: Papel')
     } else{
         console.log('Ordenador: Tijera')
@@ -28,10 +32,14 @@ function pcPlay(){
 pcPlay();
 
 
+//Eleccion del jugador
+
 function playerPlay(){
+    
     const userOption = select.value;
-   
+    
     if(userOption === 'piedra'){
+        
         console.log('Player: Piedra')
     }else if(userOption === 'papel'){
         console.log('Player: Papel')
@@ -41,16 +49,71 @@ function playerPlay(){
     }
 }
 
-playerPlay();
+button.addEventListener ('click', playerPlay);
+
+
+
+//contadores
+
+
+let tries = 0;
+
+function counterPlayer (){
+    tries += 1;
+    player.innerHTML = ` Jugador: ${tries} `;
+   
+}
+
+
+let triesPc = 0;
+
+function counterPc (){
+    triesPc += 1;
+    pc.innerHTML = ` Computadora: ${tries} `;
+}
+
 
 
 function game (){
-    if ( userOption === 'piedra' && randomNumber < 3){
-        console.log ('empate')
-    }
+    
+    const userOption = select.value;
+
+    
+    if ( userOption === 'piedra' && randomNumber <= 3){         response.innerHTML = 'Empate.';    
+
+    }else if (userOption === 'papel' && randomNumber <= 3 ){
+        response.innerHTML = '¡Has Ganado!'; 
+        counterPlayer();
+
+    }else if (userOption === 'tijera' && randomNumber <= 3 ){
+        response.innerHTML = '¡Has Perdido!'
+        counterPc ();
+
+    }else if(userOption === 'piedra' && randomNumber <= 6){
+        response.innerHTML = '¡Has Perdido!'
+        counterPc ();
+
+    }else if(userOption === 'papel' && randomNumber <= 6){
+        response.innerHTML = 'Empate.'; 
+
+    }else if(userOption === 'tijera' && randomNumber <= 6){
+        response.innerHTML = '¡Has Ganado!';
+        counterPlayer(); 
+    }  
 }
 
-game();
+button.addEventListener ('click', game);
+
+
+
+
+
+
+
+
+
+
+// game();
 
 // function handleClickUpdate(event){
 //     event.preventDefault();
